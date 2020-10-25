36eb2efdde1cf5553a3e4880556958bc6aa819aa
mineshpatel1 <neshpatel@fb.com>
2020-10-25 16:46:09 +0000

    Added missing location and some new ingredients.

--- Recipes/db_utils/data.py
+++ Recipes/db_utils/data.py
@@ -23,6 +23,18 @@ OPERATORS = {
     'ne': operator.ne,
 }
 
+# Maps Spoonacular unit names to unit IDs
+API_UNIT_MAPPING = {
+    'serving': 2,
+}
+
+API_CATEGORY_MAPPING = {
+    'Produce': 1,
+    'Beverages': 7,
+}
+
+API_IMAGE_PREFIX = 'https://spoonacular.com/cdn/ingredients_500x500/'
+
 class Unit:
     @staticmethod
     def from_dict(unit_dict):
@@ -39,6 +51,19 @@ class Ingredient:
     def from_dict(ingredient_dict):
         return Ingredient(ingredient_dict['id'], **ingredient_dict)
 
+    @staticmethod
+    def new_from_api(ingredient_id, api_dict):
+        return Ingredient(
+            ingredient_id=ingredient_id,
+            name=api_dict['original'],
+            category_id=API_CATEGORY_MAPPING.get(api_dict['aisle'], 1),
+            unit_id=API_UNIT_MAPPING[api_dict['unit']],
+            unit_size=int(api_dict['amount']),
+            api_id=api_dict['id'],
+            api_image_name=api_dict['image'],
+            image_aspect_ratio=get_image_aspect_ratio(f"{API_IMAGE_PREFIX}{api_dict['image']}"),
+        )
+
     def __init__(
         self,
         ingredient_id,
@@ -46,11 +71,11 @@ class Ingredient:
         unit_size,
         unit_id,
         category_id,
+        location_id = None,
         image_url = None,
         api_id = None,
         api_image_name = None,
         image_aspect_ratio = None,
-        location_id = None,
         *args, **kwargs
     ):
         self.id = ingredient_id
@@ -58,9 +83,9 @@ class Ingredient:
         self.unit_size = unit_size
         self.unit_id = unit_id
         self.category_id = category_id
+        self.location_id = location_id
         self.api_id = api_id
         self.image_aspect_ratio = image_aspect_ratio
-        self.location_id = location_id
         self._image_url = image_url
         self._api_image_name = api_image_name
 
@@ -76,7 +101,7 @@ class Ingredient:
         if self._image_url:
             return self._image_url
         elif self._api_image_name:
-            return f"https://spoonacular.com/cdn/ingredients_500x500/{self._api_image_name}"
+            return f"{API_IMAGE_PREFIX}{self._api_image_name}"
 
     @property
     def dict(self):
@@ -149,10 +174,10 @@ class Location:
         }
 
         if self.aisle_number:
-        out['aisle_number'] = self.aisle_number
+            out['aisle_number'] = self.aisle_number
 
         if self.associated_categories:
-        out['associated_categories'] = self.associated_categories
+            out['associated_categories'] = self.associated_categories
 
         return out
 
@@ -199,60 +224,85 @@ class Location:
         return self._compare(other, 'eq')
 
 
-    def _update_items(new_items, old_items):
-        for new_item in new_items:
-            for old_item in old_items:
-                if new_item.id == old_item['id']:
-                    old_item.update(new_item.dict)
+def _update_items(new_items, old_items):
+    for new_item in new_items:
+        found = False
+        for old_item in old_items:
+            if new_item.id == old_item['id']:
+                found = True
+                old_item.update(new_item.dict)
+
+        if not found:
+            old_items.append(new_item.dict)
 
 
-    def get_api_key():
-        api_key = get_config('SPOONACULAR_API_KEY')
-        if not api_key:
-            raise ValueError("Need to configure SPOONACULAR API KEY")
-        return api_key
+def get_max_ingredient_id():
+    data = load_json(INGREDIENTS_FILE)
+    ingredient_ids = []
+    for _ingredient in data['ingredients']:
+        ingredient_ids.append(_ingredient['id'])
+    return max(ingredient_ids)
 
 
-    def update_ingredients(ingredients=(), data_file=INGREDIENTS_FILE):
+def get_api_key():
+    api_key = get_config('SPOONACULAR_API_KEY')
+    if not api_key:
+        raise ValueError("Need to configure SPOONACULAR API KEY")
+    return api_key
+
+
+def update_ingredients(ingredients=(), data_file=INGREDIENTS_FILE):
     data = load_json(data_file)
     _update_items(ingredients, data['ingredients'])
     data['ingredients'] = sorted(data['ingredients'], key=lambda i: i['name'])
     save_json(data, data_file)
 
 
-    def update_locations(locations=(), data_file=LOCATIONS_FILE):
-        data = load_json(data_file)
-        _update_items(locations, data)
-        
-        _locations = sorted([Location.from_dict(l) for l in data])
-        data = [l.dict for l in _locations]
-        save_json(data, data_file)
+def update_locations(locations=(), data_file=LOCATIONS_FILE):
+    data = load_json(data_file)
+    _update_items(locations, data)
+    
+    _locations = sorted([Location.from_dict(l) for l in data])
+    data = [l.dict for l in _locations]
+    save_json(data, data_file)
 
 
-    def parse_ingredients(ingredient_list):
-        base_url = f'https://api.spoonacular.com/recipes/parseIngredients?apiKey={get_api_key()}'
-        res = requests.post(base_url, {
-            'ingredientList': '\n'.join(ingredient_list),
-            'servings': 1,
-            'includeNutrition': False,
-        })
-        return res.json()
+def get_ingredients_from_api(ingredient_list):
+    base_url = f'https://api.spoonacular.com/recipes/parseIngredients?apiKey={get_api_key()}'
+    res = requests.post(base_url, {
+        'ingredientList': '\n'.join(ingredient_list),
+        'servings': 1,
+        'includeNutrition': False,
+    })
+    return res.json()
 
 
-    def lookup(item_id, items):
-        match_ingredient = list(filter(lambda i: i.id == item_id, items))
-        try:
-            return match_ingredient[0]
-        except IndexError:
-            log.warning(f'Item with ID {item_id} cannot be found.')
-            return None
+def lookup(item_id, items):
+    match_ingredient = list(filter(lambda i: i.id == item_id, items))
+    try:
+        return match_ingredient[0]
+    except IndexError:
+        log.warning(f'Item with ID {item_id} cannot be found.')
+        return None
 
 
-    def get_image_aspect_ratio(uri):
-        r = requests.get(uri)
-        image = Image.open(BytesIO(r.content))
-        image_size = image.size
-        return image_size[0] / image_size[1]
+def get_image_aspect_ratio(uri):
+    r = requests.get(uri)
+    image = Image.open(BytesIO(r.content))
+    image_size = image.size
+    return image_size[0] / image_size[1]
+
+
+def add_ingredients_from_api(new_ingredients):
+    max_id = get_max_ingredient_id() + 1
+    ingredients = []
+    for api_dict in get_ingredients_from_api(new_ingredients):
+        ingredients.append(
+            Ingredient.new_from_api(max_id, api_dict),
+        )
+        max_id += 1
+
+    update_ingredients(ingredients)
 
 
 def main():
