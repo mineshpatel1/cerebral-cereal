32c0702b416f1db5ec162e3de7c5489492db67ea
mineshpatel1 <neshpatel@fb.com>
2020-11-01 13:16:02 +0000

    Revert Python DB Utils file.

--- LearnTamil/db_utils/phrase_data.py
+++ LearnTamil/db_utils/phrase_data.py
@@ -532,29 +532,7 @@ def get_next_phrases(n=50):
 
 def main():
 
-    phrases = []
-
-    phrase = get_phrase_by_key(61)
-    phrase.original = 'What is your name? [i]'
-    phrase.translation = 'Un peyar enna?'
-    phrases.append(phrase)
-    
-    phrase = get_phrase_by_key(76)
-    phrase.original = 'Where do you stay? [i]'
-    phrases.append(phrase)
-
-    phrase = get_phrase_by_key(18)
-    phrase.translation = 'Magan'
-    phrases.append(phrase)
-
-    phrase = get_phrase_by_key(19)
-    phrase.translation = 'PoNNu'
-    phrases.append(phrase)
-
-    update_data(phrases)
-
-    for p in phrases:
-        p.save_mp3()
+    pass
 
 
 if __name__ == '__main__':
